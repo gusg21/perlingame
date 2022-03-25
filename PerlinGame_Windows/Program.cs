59053808c0501e700941f6304047e218b5aa470e
[JOYCE PC] Angus Goucher <gus.goucher@gmail.com>
2022-03-25 11:31:41 -0400

    created repository and monogame project

---
+++ PerlinGame_Windows/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace PerlinGame_Windows
+{
+    public static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            using (var game = new Game1())
+                game.Run();
+        }
+    }
+}
